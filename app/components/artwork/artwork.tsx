062c5a062f8a850edce0e6f024a210b8615b9d06
Leo Vega <leonardo.vega@globant.com>
2019-02-06 13:38:39 -0300

    atwork component

--- app/components/artwork/artwork.tsx
+++ app/components/artwork/artwork.tsx
@@ -1,11 +1,20 @@
 import React, { Component, ReactNode } from "react";
+import { Grid } from "@material-ui/core";
 
 type Props = {
-  children: ReactNode
+  children: {
+    media: ReactNode
+    icon: ReactNode
+
+  }
 }
 
 export class Artwork extends Component<Props> {
   render() {
-    return <div className="card">{this.props.children}</div>
+    return <Grid className="artwork-container">
+      <Grid className="artwork-pic">{media}</Grid>
+      <Grid className="artwork-content"></Grid>
+      <Grid></Grid>
+      </Grid>
   }
 }
