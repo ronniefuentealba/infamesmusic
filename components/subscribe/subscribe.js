5608a87e8d8356fc929296d7399ed85592a605ad
Leonardo Vega <leovp17@gmail.com>
2020-08-07 13:50:01 -0400

    Subscribe  experience agregado

--- components/subscribe/subscribe.js
+++ components/subscribe/subscribe.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import Mailchimp from 'react-mailchimp-form';
 import "./subscribe.scss";
 
+import { toast, ToastContainer } from 'react-nextjs-toast'
+
+const showSubscribeForm = () => {
+  toast.notify(<SubscribeWrapper />, {
+    duration: 60,
+    title: " ",
+  })
+}
+
+const closeSubscribeForm = () => {
+  toast.remove()
+}
+
+const SubscribeWrapper = () => {
+  return(
+    <>
+      <Mailchimp
+        action='https://infames.us9.list-manage.com/subscribe/post?u=43846d510d3915799fdeabfb3&amp;id=65762b6a76'
+        fields={[
+          {
+            name: 'EMAIL',
+            placeholder: 'Email',
+            type: 'email',
+            required: true
+          }
+        ]}
+        messages = {
+          {
+            sending: "Enviando...",
+            success: "Gracias por subscribirte!",
+            error: "Se ha producido un error interno inesperado..",
+            empty: "Debes escribir un correo electrónico.",
+            duplicate: "Demasiados intentos de suscripción para esta dirección de correo electrónico",
+            button: "Subscribir!"
+          }
+        }
+        />
+        <button onClick={closeSubscribeForm}>Cerrar</button>
+      </>
+  )
+}
 const Subscribe = () => {
   return (
     <div className="subscribe">
-      <p>Subscribe</p>
+      <button onClick={showSubscribeForm}>Subscribe</button>
       <p>Recibe noticias y singles directamente en tu email.</p>
-      <Mailchimp
-          action='https://infames.us9.list-manage.com/subscribe/post?u=43846d510d3915799fdeabfb3&amp;id=65762b6a76'
-          fields={[
-            {
-              name: 'EMAIL',
-              placeholder: 'Email',
-              type: 'email',
-              required: true
-            }
-          ]}
-          />
+      <ToastContainer align={"left"} />
     </div>
       );
 };
